06964ae398c81861d3d8cb0356df041ad8ae107a
daviddc <dedc_011596@hotmail.com>
2019-11-19 14:54:32 -0600

    creación parte administrativa v1

---
+++ apps/page/templatetags/customtags.py
@@ -0,0 +1,49 @@
+# Django
+from django import template
+from django.utils import timezone
+# App page
+from ..models import Secciones, Asesorias_Tramites, Noticias, Eventos
+register = template.Library()
+
+@register.inclusion_tag('page/secciones/tag/secciontag.html')
+def secciones(sec_for):
+    if sec_for == 'public':
+        secciones = Secciones.objects.filter(is_published = True)
+    if sec_for == 'admin':
+        secciones = Secciones.objects.filter(is_disabled = False)
+    return {'secciones' : secciones, 'sec_for': sec_for}
+
+@register.inclusion_tag('page/asesorias/tag/asesoriastag.html')
+def asesorias(ase_for):
+    if ase_for == 'public':
+        asesorias = Asesorias_Tramites.objects.filter(is_published = True)
+    if ase_for == 'admin':
+        asesorias = Asesorias_Tramites.objects.filter(is_disabled = False)
+    return {'asesorias' : asesorias, 'ase_for': ase_for}
+
+@register.inclusion_tag('page/noticias/tag/noticiastag.html')
+def noticias(not_for):
+    if not_for == 'public':
+        noticias = Noticias.objects.filter(is_published = True)
+    if not_for == 'admin':
+        noticias = Noticias.objects.filter(is_disabled = False)
+    return {'noticias' : noticias, 'not_for': not_for}
+
+@register.inclusion_tag('page/eventos/tag/eventostag.html')
+def eventos(eve_for):
+    date_time = timezone.localtime()
+    print(date_time.strftime("%H:%M:%S"))
+    print('12:10:00' >= date_time.strftime("%H:%M:%S"))
+    if eve_for == 'public':
+        eventos = Eventos.objects.filter(is_published = True, fecha__gte = date_time.date(), hora__gte = date_time.strftime("%H:%M:%S"))
+    if eve_for == 'admin':
+        eventos = Eventos.objects.filter(is_disabled = False, fecha__gte = date_time.date(), hora__gte = date_time.strftime("%H:%M:%S"))
+    print(eventos)
+    return {'eventos' : eventos, 'eve_for': eve_for}
+
+@register.filter
+def rightOrLeft(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
--- esencia_landingpage/urls.py
+++ esencia_landingpage/urls.py
@@ -1,27 +1,12 @@
-"""esencia_landingpage URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-from apps.views import main
-from apps.page.views import vLogin, vLogout, vRegistroEventos
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from apps.page.views import main
+from apps.page.views import vLogin, vLogout
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', main),
-    path('evento/agregar', vRegistroEventos, name = 'rEvento'),
+    path('page/', include('apps.page.urls', namespace = 'page')),
     path('login/', vLogin, name = 'login'),
     path('logout/', vLogout, name = 'logout'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
